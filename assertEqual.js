039fb752654810e091ee7752a669581222144bc9
Abigail Addae <abigail2addae@gmail.com>
2022-05-31 23:07:00 +0000

    added implementation for assertEqual function and added extra test cases

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,18 @@
 
 const assertEqual = function(actual, expected) {
 
+  if(actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }else {
+    console.log("Assertion Failed: " + actual + " !=== " + expected);
+  }
 };
 
 // TEST CODE
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("Abi loves cats", "Abi loves cats");
+assertEqual("Abi", "Absi");
+assertEqual(22.8, 22.8);
+assertEqual(-50, 50);
