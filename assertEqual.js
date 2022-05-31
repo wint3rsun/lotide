cec38b80f35aef150155c53641001100d84152a1
Abigail Addae <abigail2addae@gmail.com>
2022-05-31 23:14:33 +0000

    added emoji's to output for clarity in assertEquals.js

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@
 
 const assertEqual = function(actual, expected) {
 
-  if(actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
-  }else {
-    console.log("Assertion Failed: " + actual + " !=== " + expected);
+  if (actual === expected) {
+    console.log("✅✅✅🥰🤪✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌🤬🤯❌❌❌Assertion Failed: " + actual + " !=== " + expected);
   }
 };
 
