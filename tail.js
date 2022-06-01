bd74d811cb2ad4e73ff7c7eb38350b3ef607426b
Abigail Addae <abigail2addae@gmail.com>
2022-06-01 14:04:53 +0000

    added implentation for tail function, and test cases for tail.js

--- tail.js
+++ tail.js
@@ -10,18 +10,37 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-
+  return (array.length > 1) ? array.slice(1) : [];
 };
 
 // TEST CODE
 
 // Test Case 1: Check the returned array elements
+console.log("Test Case 1: Check the returned array elements");
+console.log("----------");
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
+console.log("----------");
 
 // Test Case 2: Check original array
+console.log("Test Case 2: Check original array");
+console.log("----------");
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+console.log("----------");
+
+// Test Case 3: Array with single element
+console.log("Test Case 3: Array with single element");
+console.log("----------");
+const numbers = [1];
+assertEqual(tail(numbers).length, 0);
+console.log("----------");
+
+// Test Case 4: Empty array
+console.log("Test Case 4: Empty array");
+console.log("----------");
+assertEqual(tail([]).length, 0);
+console.log("----------");
