4587486985230375d8837c594f7cf83c68463bb1
Abigail Addae <abigail2addae@gmail.com>
2022-06-01 00:24:33 +0000

    updated head.js for eslint formatting

--- head.js
+++ head.js
@@ -11,11 +11,11 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   return array[0];
-}
+};
 
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
-assertEqual(head([ ,6,7]), );
+assertEqual(head([undefined,6,7]), undefined);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["", "Lighthouse", "Labs"]), "");
 assertEqual(head([false, true, true]), false);
