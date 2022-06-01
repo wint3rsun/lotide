71f234eeab822c58e93b18aaa142c56bd2b1d316
Abigail Addae <abigail2addae@gmail.com>
2022-06-01 21:34:59 +0000

    added function definition to eqArrays function in eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -10,8 +10,20 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   let isEqual = false;
 
+  if (arr1.length === arr2.length) {
+    let count = 0;
+    for (let i in arr1) {
+      if (arr1[i] === arr2[i]) {
+        count++;
+      }
+    }
+    if (count === arr1.length) {
+      isEqual = true;
+    }
+  }
+
   return isEqual;
-}
+};
 
 
 // Test Cases
@@ -20,4 +32,13 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["3", "2", "1"]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false);
+
+assertEqual(eqArrays([], ["1", "2", "3"]), false);
+assertEqual(eqArrays([], []), true);
+
+
+
 
