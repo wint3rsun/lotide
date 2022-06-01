2f817506cade235e40779f0bc763245641b90881
Abigail Addae <abigail2addae@gmail.com>
2022-06-01 00:14:22 +0000

    added function implementation for head function and added more test cases in head.js

--- head.js
+++ head.js
@@ -9,10 +9,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function() {
-
+const head = function(array) {
+  return array[0];
 }
 
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
+assertEqual(head([ ,6,7]), );
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["", "Lighthouse", "Labs"]), "");
+assertEqual(head([false, true, true]), false);
+assertEqual(head(["Abi"]), "Abi");
+assertEqual(head([13]), 13);
+assertEqual(head([true]), true);
+assertEqual(head([]), undefined);
