6de3cad0e19d206d1d0422fab3361e05e28c7d87
Abigail Addae <abigail2addae@gmail.com>
2022-06-02 11:36:18 +0000

    added function definition for countOnly in countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -15,8 +15,13 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
+  for (const item of allItems) {
+    if (itemsToCount[item] === true) {
+      (results[item]) ? results[item] += 1 : results[item] = 1;
+    }
+  }
   return results;
-}
+};
 
 // TEST CASES
 
