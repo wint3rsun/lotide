4eec8fe3d222a7b336a91ab39aaf2ed3c5359266
Abigail Addae <abigail2addae@gmail.com>
2022-06-02 18:10:08 +0000

    Updated asserObjectsEqual function to use inspect library for displaying

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -39,10 +39,12 @@ const eqObjects = function(obj1, obj2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
   if (eqObjects(actual, expected)) {
-    console.log("✅✅✅🥰🤪✅✅✅Assertion Passed: ", actual, "=== ", expected);
+    console.log(`✅✅✅🥰🤪✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log("❌❌❌🤬🤯❌❌❌Assertion Failed: ", actual, "!=== ", expected);
+    console.log(`❌❌❌🤬🤯❌❌❌Assertion Failed: ${inspect(actual)} !=== ${inspect(expected)}`);
   }
 };
 
