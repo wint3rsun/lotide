a4b61aecce45875abf96f5c72092342c940d8472
Abigail Addae <abigail2addae@gmail.com>
2022-06-02 11:11:40 +0000

    added skeleton for countLetters function in countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,18 @@
+// FUNCTION DEFINITIONS
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅🥰🤪✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌🤬🤯❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const countLetters = function() {
+  let count = {};
+
+};
+
+
+// TEST CASES
