2ebd9903fcbccb32dc09d8c542eb59e6b825599f
Abigail Addae <abigail2addae@gmail.com>
2022-06-06 21:04:27 +0000

    refactored eqArrays.js to export eqArrays function and separated test code to new file eqArraysTest.js in tests directory

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// Functions
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅🥰🤪✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌🤬🤯❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let isEqual = false;
 
@@ -25,20 +16,4 @@ const eqArrays = function(arr1, arr2) {
   return isEqual;
 };
 
-
-// Test Cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["3", "2", "1"]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false);
-
-assertEqual(eqArrays([], ["1", "2", "3"]), false);
-assertEqual(eqArrays([], []), true);
-
-
-
-
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// Test Cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["3", "2", "1"]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false);
+
+assertEqual(eqArrays([], ["1", "2", "3"]), false);
+assertEqual(eqArrays([], []), true);
