4ac90e10399aea20b92e7c4c58db407187a94b30
Abigail Addae <abigail2addae@gmail.com>
2022-06-07 03:02:21 +0000

    removed test cases and created respective test.js files in test directory

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,42 +1,4 @@
-// FUNCTION DEFINITIONS
-const eqArrays = function(arr1, arr2) {
-  let isEqual = false;
-
-  if (arr1.length === arr2.length) {
-    let count = 0;
-    for (let i in arr1) {
-      if (arr1[i] === arr2[i]) {
-        count++;
-      }
-    }
-    if (count === arr1.length) {
-      isEqual = true;
-    }
-  }
-  return isEqual;
-};
-
-const eqObjects = function(obj1, obj2) {
-  let isEqual = false;
-
-  let obj1Keys = Object.keys(obj1);
-  let obj2Keys = Object.keys(obj2);
-
-  if (obj1Keys.length === obj2Keys.length) {
-    let count = 0;
-    for (const key of obj1Keys) {
-      if (obj2[key] && obj1[key] === obj2[key]) {
-        count += 1;
-      } else  if (Array.isArray(obj1[key]) && Array.isArray(obj2[key]) && eqArrays(obj1[key], obj2[key])) {
-        count += 1;
-      }
-    }
-    if (count === obj1Keys.length) {
-      isEqual = true;
-    }
-  }
-  return isEqual;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -48,25 +10,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// TEST CASES
-const obj1 = {
-  name: "Abi",
-  age: 10000,
-  phone: "apple"
-};
-
-const obj2 = {
-  age: 10000,
-  phone: "apple",
-  name: "Abi"
-};
-
-const obj3 = {
-  name: "Artemis",
-  species: "cat",
-  age: 50
-};
-
-assertObjectsEqual(obj1,obj2);
-assertObjectsEqual(obj1,obj3);
-assertObjectsEqual(obj3, obj2);
+module.exports = eqObjects;
--- eqObjects.js
+++ eqObjects.js
@@ -1,31 +1,6 @@
-// FUNCTION DEFINITIONS
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅🥰🤪✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌🤬🤯❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  let isEqual = false;
-
-  if (arr1.length === arr2.length) {
-    let count = 0;
-    for (let i in arr1) {
-      if (arr1[i] === arr2[i]) {
-        count++;
-      }
-    }
-    if (count === arr1.length) {
-      isEqual = true;
-    }
-  }
-  return isEqual;
-};
-
-//Returns true if both objects have identical keys with identical values
-//Otherwise you get false!
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const eqObjects = function(obj1, obj2) {
   let isEqual = false;
@@ -49,42 +24,5 @@ const eqObjects = function(obj1, obj2) {
   return isEqual;
 };
 
+module.exports = eqObjects;
 
-// TEST CASES
-const ab = {a: "1", b: "2"};
-const ba = {b: "2", a: "1"};
-const abc = {a: "1", b: "2", c: "3"};
-const ac = {a: "1", c: "3"};
-const abN = {a: 1, b: 2};
-const baN = {b: 2, a: 1};
-
-const cd = { c: "1", d: ["2", 3]};
-const dc = { d: ["2", 3], c: "1"};
-const cd2 = {c: "1", d: ["2", 3, 4]};
-
-console.log("Object 1: ", ab, "\nObject 2: ", ba);
-assertEqual(eqObjects(ab, ba), true);
-console.log("----------------------------");
-
-console.log("Object 1: ", ab, " \nObject 2:", abc);
-assertEqual(eqObjects(ab, abc), false);
-console.log("----------------------------");
-
-console.log("Object 1: ", ab, " \nObject 2:", ac);
-assertEqual(eqObjects(ab, ac), false);
-console.log("----------------------------");
-
-console.log("Object 1: ", ab, "\nObject 2: ", abN);
-assertEqual(eqObjects(ab, abN), false);
-console.log("----------------------------");
-
-console.log("Object 1: ", abN, "\nObject 2: ", baN);
-assertEqual(eqObjects(abN, baN), true);
-
-console.log("Object 1: ", cd, "\nObject 2: ", dc);
-assertEqual(eqObjects(cd, dc), true);
-console.log("----------------------------");
-
-console.log("Object 1: ", cd, "\nObject 2: ", cd2);
-assertEqual(eqObjects(cd, cd2), false);
-console.log("----------------------------");
